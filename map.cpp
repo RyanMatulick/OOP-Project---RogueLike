0a0d48f14afaa04c02641d9693d39b87ffc634a5
JackMansfield <jackmansfield8000@gmail.com>
2016-05-06 09:50:54 +0930

    map class
    blank files for the map class

---
+++ map.cpp
@@ -0,0 +1,30 @@
+//map.cpp
+
+#include map.h
+
+using namespace std;
+
+map::map()
+{
+
+}
+
+void map::GenerateMap()
+{
+
+}
+
+void map::MapUpdate()
+{
+
+}
+
+void map::DrawMap()
+{
+
+}
+
+map::~map()
+{
+
+}
---
+++ map.h
@@ -0,0 +1,17 @@
+//map.h
+
+using namespace std;
+
+class map
+{
+public:
+
+    map();
+    void GenerateMap(); // occurs once per room
+    void MapUpdate(); // update current map from enemy and player inputs
+    void DrawMap(); // occurs after player input
+    ~map();
+
+private:
+    char * Map[80,20];
+}
