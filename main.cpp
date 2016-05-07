d4806d9230be91f96f954cbfd3f923717fedc8fe
Ryan Matulick <Ryan.matulick@gmail.com>
2016-05-08 00:47:54 +0930

    Visual Studio solution, Test Map, Fixed Extra Functions
    Just filled out a massive description and git hub crashed.
    so mad. anyway.
    
    1) Added a Visual Studio solution that edits the files directly. If you
    want to use this solution you will have to go to project > Properties>
    Configuration Properties > c/c++ > Precompiled Headers, and change the
    "Precompiled Header" setting to "Not Using Precompiled Headers" option.
    this means you will not have to include "stdafx.h" for it to run.
    
    2) Added the base static test map to the map class, and a corresponding
    print function. This is currently displayed directly underneath the
    random map.
    
    3) Changed the getch function , now getKey() , to work on both windows
    and Linux, can be used void or the return can be assigned to a variable.
    the clear_screen() function is also fixed.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
 #include "map.h"
-#include "map.cpp"
+#include "OS_Functions.h" // getch and 
+//#include "map.cpp"
 
 using namespace std;
 
 int main()
 {
-    map * theMap;
-    theMap = new map(10,10);
-    theMap->generateMap();
-    theMap->drawMap();
+
+	map * theMap;
+	theMap = new map(10,10);
+	theMap->generateMap();
+	theMap->drawMap();
+	
+
+
+	// getKey Example
+	char key = getKey();
+	cout << "You inputted: " << key << endl;
+	//Test Map
+	cout << "Before Test Map" << endl;
+	map * testMap;
+	testMap = new map(10, 10);
+	testMap->printTestMap();
+	cout << "After TestMap" <<endl;
+
+	getKey(); // needed on VS to see final output, pauses until keypress
 }
--- map.cpp
+++ map.cpp
@@ -1,7 +1,7 @@
 //map.cpp
 //map generates, updates and displays the game map, whihc is stored as a 80x20 array or characters
 //  #=wall  " "=blanck
-
+#include <iostream>
 #include "map.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,120 +13,120 @@ char * Map[10][10];
 
 map::map(int inWidth, int inHeight)
 {
-    srand(time(NULL)); //seed the random number generator
-    width=inWidth;
-    height = inHeight;
-    numberOfRooms=0;
+	srand(time(NULL)); //seed the random number generator
+	width=inWidth;
+	height = inHeight;
+	numberOfRooms=0;
 }
 
 void map::initializeMap() // initalize Map with blanks
 {
-    for(int i=0; i<width; i++)
-    {
-        for(int j=0; j<height; j++)
-        {
-            Map[i][j] = " ";
-        }
-    }
+	for(int i=0; i<width; i++)
+	{
+		for(int j=0; j<height; j++)
+		{
+			Map[i][j] = " ";
+		}
+	}
 
-    int firstCell = rand()%width; //place # in a random cell in the top row
-    Map[0][firstCell] = "#";
+	int firstCell = rand()%width; //place # in a random cell in the top row
+	Map[0][firstCell] = "#";
 
-    for(int k = firstCell+1; k<width; k++) //fill out top row from the first cell
-    {
+	for(int k = firstCell+1; k<width; k++) //fill out top row from the first cell
+	{
 
-        if(generateRandomNumber()>3 && Map[0][k-1]=="#")
-        {
-            Map[0][k] = "#";
-            numberOfRooms++;
-        }
-        else
-            Map[0][k] = " ";
-    }
+		if(generateRandomNumber()>3 && Map[0][k-1]=="#")
+		{
+			Map[0][k] = "#";
+			numberOfRooms++;
+		}
+		else
+			Map[0][k] = " ";
+	}
 }
 
 void map::fillMap()
 {
-    //# in the map represent rooms, " " are not rooms
-    //algorithm fills up the map with randomly generated rooms that touch
-    //each other- no isolated rooms
-    //this function will exectue until there
+	//# in the map represent rooms, " " are not rooms
+	//algorithm fills up the map with randomly generated rooms that touch
+	//each other- no isolated rooms
+	//this function will exectue until there
 
-    for(int i =1; i<height; i++) //fill out rest of the array
-    {
-        for(int j = 0; j<width; j++)
-        {
-            if(j>0) //dont go out of array bounds!
-            {
-                if(generateRandomNumber()>3 && ((Map[i][j-1]=="#")||(Map[i-1][j]=="#"))) //if a cell to the top or left of the current is a room, possibly v=create a new room
-                {
-                      Map[i][j] = "#";
-                      numberOfRooms++;
-                }
+	for(int i =1; i<height; i++) //fill out rest of the array
+	{
+		for(int j = 0; j<width; j++)
+		{
+			if(j>0) //dont go out of array bounds!
+			{
+				if(generateRandomNumber()>3 && ((Map[i][j-1]=="#")||(Map[i-1][j]=="#"))) //if a cell to the top or left of the current is a room, possibly v=create a new room
+				{
+					  Map[i][j] = "#";
+					  numberOfRooms++;
+				}
 
-            }
-            else
-            {
-                if(generateRandomNumber()>3 && (Map[i-1][j]=="#"))
-                {
-                    Map[i][j] = "#";
-                    numberOfRooms++;
-                }
+			}
+			else
+			{
+				if(generateRandomNumber()>3 && (Map[i-1][j]=="#"))
+				{
+					Map[i][j] = "#";
+					numberOfRooms++;
+				}
 
-            }
-            if(j<width-1 && i<height-1) //as above, but looking down and to the right
-            {
-                if(generateRandomNumber()>3 && ((Map[i][j+1]=="#") || (Map[i+1][j]=="#")))
-                   {
-                       Map[i][j] = "#";
-                       numberOfRooms++;
-                   }
-            }
+			}
+			if(j<width-1 && i<height-1) //as above, but looking down and to the right
+			{
+				if(generateRandomNumber()>3 && ((Map[i][j+1]=="#") || (Map[i+1][j]=="#")))
+				   {
+					   Map[i][j] = "#";
+					   numberOfRooms++;
+				   }
+			}
 
-        }
-    }
+		}
+	}
 }
 void map::generateMap() //fill map array with # to represent rooms that exist
 {
-    initializeMap();
+	initializeMap();
 
-    //# in the map represent rooms, " " are not rooms
-    //algorithm fills up the map with randomly generated rooms that touch
-    //each other- no isolated rooms
+	//# in the map represent rooms, " " are not rooms
+	//algorithm fills up the map with randomly generated rooms that touch
+	//each other- no isolated rooms
 
-    for(int i =1; i<height; i++) //fill out rest of the array
-    {
-        for(int j = 0; j<width; j++)
-        {
-            if(j>0) //dont go out of array bounds!
-            {
-                if(generateRandomNumber()>3 && ((Map[i][j-1]=="#")||(Map[i-1][j]=="#"))) //if a cell to the top or left of the current is a room, possibly v=create a new room
-                {
-                      Map[i][j] = "#";
-                      numberOfRooms++;
-                }
-                else
-                    Map[i][j] = " ";
-            }
-            else
-            {
-                if(generateRandomNumber()>3 && (Map[i-1][j]=="#"))
-                {
-                    Map[i][j] = "#";
-                    numberOfRooms++;
-                }
-                else
-                    Map[i][j] = " ";
-            }
+	for(int i =1; i<height; i++) //fill out rest of the array
+	{
+		for(int j = 0; j<width; j++)
+		{
+			if(j>0) //dont go out of array bounds!
+			{
+				if(generateRandomNumber()>3 && ((Map[i][j-1]=="#")||(Map[i-1][j]=="#"))) //if a cell to the top or left of the current is a room, possibly v=create a new room
+				{
+					  Map[i][j] = "#";
+					  numberOfRooms++;
+				}
+				else
+					Map[i][j] = " ";
+			}
+			else
+			{
+				if(generateRandomNumber()>3 && (Map[i-1][j]=="#"))
+				{
+					Map[i][j] = "#";
+					numberOfRooms++;
+				}
+				else
+					Map[i][j] = " ";
+			}
 
-        }
-    }
+		}
+	}
 
-    //once filled out once, repeat until we have enough rooms
-    while(numberOfRooms<((width*height)/3)) //fill map until there are at least a third space filled with rooms
-    {
-        fillMap();
-    }
+	//once filled out once, repeat until we have enough rooms
+	while(numberOfRooms<((width*height)/3)) //fill map until there are at least a third space filled with rooms
+	{
+		fillMap();
+	}
 
 }
 
@@ -137,29 +137,41 @@ void map::mapUpdate()
 
 void map::drawMap()
 {
-    for(int i =0; i<height; i++)
-    {
-        for(int j = 0; j<width; j++)
-        {
-            cout << Map[i][j];
-        }
-        cout << endl;
-    }
+	for(int i =0; i<height; i++)
+	{
+		for(int j = 0; j<width; j++)
+		{
+			cout << Map[i][j];
+		}
+		cout << endl;
+	}
+}
+
+void map::printTestMap() // TEMP
+{
+	for (int i = 0; i<20; i++)
+	{
+		for (int j = 0; j<40; j++)
+		{
+			cout << TestMap[i][j];
+		}
+		cout << endl;
+	}
 }
 
 char * map::getMapPtr()
 {
-
+	return 0;
 }
 
 //private
 int map::generateRandomNumber()
 {
-    return (rand() % 8 + 1); //return a number between 1 and 8
+	return (rand() % 8 + 1); //return a number between 1 and 8
 }
 
 
 map::~map()
 {
-    delete Map;
+	delete Map;
 }
