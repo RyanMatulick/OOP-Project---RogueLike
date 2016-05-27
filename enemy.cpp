3b33f3298316676122adfa09ee9e05c5ab061268
JackMansfield <jackmansfield8000@gmail.com>
2016-05-27 09:53:38 +0930

    delete everything
    # Conflicts:
    #	character.cpp
    #	main.cpp

--- enemy.cpp
+++
@@ -1,9 +0,0 @@
-#include "enemy.h"
-
-enemy::enemy(int iSymbol, int ixLocation, int iyLocation,int iHealth, int iAttackD):character(iSymbol, ixLocation, iyLocation, iHealth, iAttackD,"ENEMY")
-{
-}
-
-enemy::~enemy()
-{
-}
